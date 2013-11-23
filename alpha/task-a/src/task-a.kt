fb7026ebfcb9b0bed1bdcee56b04d0bca8cee116
Sergey Popov <poserg@gmail.com>
2013-11-24 01:21:34 +0400

    Resolved task A.

--- alpha/task-a/src/task-a.kt
+++ alpha/task-a/src/task-a.kt
@@ -1,3 +1,14 @@
 /**
  * Created by sergey on 23.11.13.
  */
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val line = readLine()
+    if (line !is String)
+        return
+
+    var index = line.indexOf(" ")
+    print(line.substring(0, index).toInt() + line.substring(index + 1, line.size).toInt())
+}
