aeccfebaa358d310a79e5cfea91e288e452876f8
Sergey Popov <poserg@gmail.com>
2013-12-20 02:34:11 +0400

    Added task C

---
+++ alpha/task-c/src/task-c.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by Сергей on 20.12.13.
+ */
+fun main(args: Array<String>) {
+    val line = readLine()
+    if (line !is String)
+        return
+
+    val n = line.toInt()
+}
+
+fun getResheto(n: Int) {
+    var resheto = emptyArray
+
+    for (i in 2..n) {
+
+    }
+}
