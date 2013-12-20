c490c9c188dd4c9cab7c2184413f678e130c4540
Sergey Popov <poserg@gmail.com>
2013-12-21 01:30:14 +0400

    Any solution for task-d

--- alpha/task-d/src/task-d.kt
+++ alpha/task-d/src/task-d.kt
@@ -2,6 +2,10 @@
  * Created by sergey on 20/12/13.
  */
 
+import java.util.HashSet
+import java.util.ArrayList
+import java.util.HashMap
+
 fun main(args: Array<String>) {
     var line = readLine()
 
@@ -13,7 +17,7 @@ fun main(args: Array<String>) {
     val m = nm.get(1).toInt()
 
     val vers: Array<Int?> = arrayOfNulls(m)
-    val rebra: Array<Int?> = arrayOfNulls(m)
+    val vers2: Array<Int?> = arrayOfNulls(m)
     val weight: Array<Int?> = arrayOfNulls(m)
 
     for (i in vers.indices) {
@@ -23,9 +27,82 @@ fun main(args: Array<String>) {
 
         val k = line!!.split(" ")
         vers.set(i, k.get(0).toInt())
-        rebra.set(i, k.get(1).toInt())
+        vers2.set(i, k.get(1).toInt())
         weight.set(i, k.get(2).toInt())
     }
 
+    val tops = createModel(n, vers, vers2, weight)
+    for (i in tops.indices) {
+        getPath(tops, i+1)
+    }
+}
 
+fun getPath(tops: Array<Top>, index: Int) {
+    for (i in tops)
+        i.clear()
+
+    tops.get(index - 1).getPath()
+
+    for (i in tops) {
+        print (i.weight)
+        print (" ")
+    }
+    print ("\n")
+}
+
+fun createModel(n: Int, vers:  Array<Int?>, vers2:  Array<Int?>, weight:  Array<Int?>): Array<Top> {
+    val result = Array<Top>(n, {i -> Top(i + 1)})
+    for (i in vers.indices) {
+        val top = result.get(vers.get(i)!! - 1)
+        val otherTop = result.get(vers2.get(i)!! - 1)
+        val weight = weight.get(i)
+
+        top.addEdge(otherTop, weight!!)
+    }
+    return result
+}
+
+class Top (val index: Int) {
+
+    val edges = HashSet<Edge>()
+    var isWas = false
+    var weight: Int = -1
+
+    fun addEdge(otherTop: Top, weight: Int) {
+        val edge = Edge(this, otherTop, weight)
+        edges.add(edge)
+    }
+
+    fun clear() {
+        weight = -1
+        isWas = false
+    }
+
+    fun getPath() {
+        if (weight == -1) {
+            weight = 0
+        }
+        isWas = true
+        // print("start\nedge.weight = ")
+        for (edge in edges.filter { i -> !i.getOtherTop(this).isWas }.sortBy { it.weight }) {
+            // print(edge.weight)
+            // print("\n")
+            val otherTop = edge.getOtherTop(this)
+            if (otherTop.weight == -1 || otherTop.weight > weight + edge.weight) {
+                otherTop.weight = weight + edge.weight
+            }
+        }
+        for (edge in edges.filter { i -> !i.getOtherTop(this).isWas }.sortBy { it.weight }) {
+            edge.getOtherTop(this).getPath()
+        }
+    }
+}
+
+class Edge (val top1: Top, val top2: Top, val weight: Int){
+    fun getOtherTop(curTop: Top): Top {
+        if (curTop.index == top1.index)
+            return top2
+        else
+            return top1
+    }
 }
