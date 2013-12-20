b544bed93e463521a914e7673d7a9ea2116cd4dd
Sergey Popov <poserg@gmail.com>
2013-12-20 17:13:49 +0400

    Tasc C solved

--- alpha/task-c/src/task-c.kt
+++ alpha/task-c/src/task-c.kt
@@ -7,12 +7,37 @@ fun main(args: Array<String>) {
         return
 
     val n = line.toInt()
+
+    val resheto = getResheto(n)
+
+    var s = 0
+    for (i in 0..n/2) {
+        if (resheto.get(i) as Boolean) {
+            if (resheto.get(n - i) as Boolean)
+                s++
+        }
+    }
+    print(s)
 }
 
-fun getResheto(n: Int) {
-    var resheto = emptyArray
+fun getResheto(n: Int): Array<Boolean?> {
+    var resheto: Array<Boolean?> = arrayOfNulls(n)
 
-    for (i in 2..n) {
+    for (i in 0..resheto.size - 1)
+        resheto.set(i, true)
 
+    if (resheto.size > 0)
+        resheto.set(0, false)
+    if (resheto.size > 1)
+        resheto.set(1, false)
+
+    for (i in 2..resheto.size - 1) {
+        if (resheto.get(i) as Boolean) {
+            for (j in i*i..n - 1 step i) {
+                resheto.set(j, false)
+            }
+        }
     }
+
+    return resheto
 }
