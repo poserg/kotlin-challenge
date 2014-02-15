7b393d8781ae9c651b9ba5ddee830f6201c33262
Sergey Popov <poserg@gmail.com>
2014-02-15 13:15:25 +0400

    Task E. New Instance

--- alpha/task-e/src/task-e.kt
+++ alpha/task-e/src/task-e.kt
@@ -6,6 +6,7 @@ import com.sun.xml.internal.fastinfoset.util.StringArray
 import java.util.ArrayList
 
 fun main(args: Array<String>) {
+    while (true) {
     val s = readLine()
     if (s !is String)
         return
@@ -20,16 +21,43 @@ fun main(args: Array<String>) {
         return
 
     var c2 = s2.toInt()
-    var str2 = readLine()
+    val str2 = readLine()
+    if (str2 !is String)
+        return
 
     if (c1 > c2) {
         run(str2, c2, str1)
     } else {
         run(str1, c1, str2)
     }
+
+        print("\n")
+    }
 }
 
 fun run(str1 : String, c1 : Int, str2 : String) {
+    fun getLines(strArr: ArrayList<String>): ArrayList<String> {
+        val result = ArrayList<String>()
+        var m = strArr[0]
+        var k = m.lastIndexOf(" ")
+        if (k >= 0) {
+            result.add(m.substring(0, k))
+            result.add(m.substring(m.indexOf(" ") + 1))
+
+            var i = 1;
+            while (i < strArr.size()) {
+                m = strArr[i]
+                val s = m.substring(m.indexOf(" ") + 1)
+                result.add(s)
+                i++
+            }
+        }
+
+        for (i in result)
+            print (i + "t\n")
+        return result
+    }
+
     var test = ArrayList<String>()
     test.add(str1)
     var c = c1
@@ -52,22 +80,6 @@ fun run(str1 : String, c1 : Int, str2 : String) {
     }
 
 
-    for (t in test) {
-        print (t + "k\n")
-    }
+    print ("0")
 }
 
-fun getLines(strArr: ArrayList<String>): ArrayList<String> {
-    val result = ArrayList<String>()
-    var m = strArr[0]
-    result.add(m.substring(0, m.lastIndexOf(" ")))
-    result.add(m.substring(m.indexOf(" ") + 1))
-
-    var i = 1;
-    while (i < strArr.size()) {
-        m = strArr[i]
-        result.add(m.substring(m.indexOf(" ")))
-        i++
-    }
-    return result
-}
